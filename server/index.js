2b4beffb29fe08457a4cc40019273480b2623857
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-10-13 20:02:58 -0400

    added google strategy and auth flow

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
+//app config//
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: 'oauth/google/callback'
+    },
+      (accessToken, refreshToken, profile, done) => {
+        console.log('Access Token:',accessToken);
+        console.log('Refresh Token:',refreshToken);
+        console.log('Profile:',profile);
+      }
+  )
+);
 
-app.get('/', (req, res) => {
-      res.send('HI');
-});
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+console.log('feedback is running');
