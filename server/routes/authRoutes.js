0cfe11909b3bd2b20fc5c7b7e77177fa33ce4219
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-10-26 17:35:08 -0400

    added logout functionality and completed main auth flow

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,18 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+  //route to "sign in with ggogle"//
   app.get('/auth/google', passport.authenticate('google', {
       scope: ['profile', 'email']
     })
   );
-
+  //callback after authentication//
   app.get('/auth/google/callback',passport.authenticate('google'));
+  //logout//
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send("You logged out");
+  });
 
   app.get('/api/currentuser', (req,res) => {
     res.send(req.user);
