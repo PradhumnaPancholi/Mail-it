2405dc1008cb0eed51d7eabe0e844501ce1e282f
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-10-29 15:36:03 -0400

    rebranding and refactor for production

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+if(process.env.NODE_ENV === 'production'){
+  //we are in production env//
+  module.exports = require('./prod');
+}else{
+  //we are in development env//
+
+  module.exports = require('./dev');
+}
