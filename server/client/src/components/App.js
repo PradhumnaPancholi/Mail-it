13fde56dcb91b08cd85999aa0761519c763127ac
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-11-24 14:14:24 -0500

    configured basuc routes, header component and matrialize-css

---
+++ server/client/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header';
+
+//temp components to verify routes//
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>Add a new survey</h2>
+const Landing = () => <h2>Landing</h2>
+
+const App = () => {
+  return(
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+
+
+export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import reducers from './reducers';
+import 'materialize-css/dist/css/materialize.min.css'; 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDom.render(
+    <Provider store={store}><App /></Provider>,
+    document.querySelector('#root')
+);
