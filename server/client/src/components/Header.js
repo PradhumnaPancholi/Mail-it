501cdf8586798bf49b1ac346c45d419a617a5a95
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-11-25 23:56:39 -0500

    redux thunk config

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component{
   render(){
     return(
         <nav>
-          <div className="nav-wrapper">
+          <div className="nav-wrapper blue-grey darken-3">
             <a className="left brand-logo">
             FeedBack
             </a>
