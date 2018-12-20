7f8e37f93405dfd5233a13d4f1e57484263a9975
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-12-20 14:49:49 -0500

    added actioncreator to handle token

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
-export const fetchUser = () => async (dispatch) => {
+export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/currentuser');
     dispatch({type: FETCH_USER, payload: res.data});
   };
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe');
+  dispatch({type: FETCH_USER, payload: res.data}); 
+}
