22b3e1ac371bcdce8b67e314fcb88972abbbccb7
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-12-27 17:33:50 -0500

    added reipientSchema and connected with surveys as subdoc, added fields in surveyShema to setup relation with user and more

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const recipientSchema = require('./Recipients');
 
 const surveySchema = new Schema({
   title: String,
   subject: String,
   body: String,
-  recipients: [String],
+  recipients: [recipientSchema],
   positive: { type: Number, default: 0},
-  negative: { type: Number, default: 0}
+  negative: { type: Number, default: 0},
+  _user: { type: Schema.Types.ObjectId, ref: 'User'},
+  dateSent: Date,
+  lastResponse: Date
 });
 
 mongoose.model('surveys', surveySchema);
