00553d919de26c2e91ebe6bccec2ca82fe9e904f
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-01-01 14:14:59 -0500

    deleted setupProxy.js and applied in an efficient way to work on prod as well as dev"

--- server/client/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-const dev_target =  'http://localhost:5000/'
-
-module.exports = function(app) {
-  app.use(proxy('/auth/google', { target: dev_target })),
-  app.use(proxy('/api/*', { target: dev_target }));
-};
