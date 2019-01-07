370537d3818c49bf9305d7a1c77a83e2337e18ce
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-01-07 17:05:20 -0500

    added 'exports' to user Model-debug from log

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,6 +1,6 @@
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
-const keys = require(../config/keys);
+const keys = require('../config/keys');
 
 class Mailer extends helper.Mail{
 
